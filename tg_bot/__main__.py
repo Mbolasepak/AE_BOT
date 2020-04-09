947e76a83be23fc47ee1ac1c335fa70c2c23ca42
THEALIFHAKER1 <56091627+THEALIFHAKER1@users.noreply.github.com>
2020-04-10 03:49:43 +0800

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,13 +18,13 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hey there! My name is {} - I'm here to help you manage your groups!
-Click /help  to find out more about how to use me to my full potential.
+Hey there! My name is {}! - I'm here to help you manage your groups!
+Click /help to find out more about how to use me to my full potential.
 
-im a group manager bot maintained by  [this person](tg://user?id={}).
+im a group manager bot maintained by  [this person](t.me/THEALIFHAKER1).
 
 **#AE1**
-[Source Code](https://github.com/THEALIFHAKER1/AE_BOT)
+[Source Code](buttonurl:https://github.com/THEALIFHAKER1/AE_BOT)
 """
 
 HELP_STRINGS = """
